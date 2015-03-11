4426a92c57180904c0d7be1967b539e05ab9bd95
Joel Hansson <joel.hansson@ecraft.com>
2015-03-11 16:29:52 +0100

    add git completion

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,6 +3,11 @@ sys_type=`uname`
 if [ "$sys_type" == "Darwin" ]; then
   PS1='\[\033[01;32m\]\u@\h:\[\033[01;34m\]\W'
   PS1=$PS1'\[\033[00m\]'
+  if [ -f ~/.git-prompt.sh ] ; then
+    export GIT_PS1_SHOWDIRTYSTATE=1
+    . ~/.git-prompt.sh
+    PS1=$PS1'$(__git_ps1 " (%s)")'
+  fi
   if [ "`id -u`" -eq 0 ]; then
    PS1=$PS1'# '
   else
