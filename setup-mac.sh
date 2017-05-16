bc83b35200aea1f189b9e93ae67db9901fd55b2c
Joel Hansson <joel.hansson@ecraft.com>
2017-05-16 16:09:06 +0200

    do not rearrange spaces

--- setup-mac.sh
+++ setup-mac.sh
@@ -35,6 +35,7 @@ defaults write com.apple.dock springboard-page-duration -float 0
 defaults write com.apple.finder DisableAllAnimations -bool true
 defaults write com.apple.Mail DisableSendAnimations -bool true
 defaults write com.apple.Mail DisableReplyAnimations -bool true
+defaults write com.apple.dock mru-spaces -bool false
 )
 killall Dock
 
