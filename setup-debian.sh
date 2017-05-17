2ec7a15f94ca1787c2b9ffda446451a15e26559d
Joel Hansson <joel.hansson@ecraft.com>
2017-05-17 09:51:55 +0200

    adds a setup-debian script

---
+++ setup-debian.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+confirm()
+{
+  echo "$@"
+  echo "continue with enter, ctrl-c to abort"
+  read _
+}
+
+confirm "have you setup your ssh keys?"
+confirm "lets install some packages!"
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y \
+  mosh vim-nox procps tcpdump nmap build-essential \
+  tmux unattended-upgrades git zsh
+
+confirm "install docker from https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository"
+confirm "install chruby, ruby-install and nvm, google site:github.com for 'em!"
