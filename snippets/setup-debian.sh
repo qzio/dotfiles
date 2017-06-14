ce470dfe4a0d70f2fcd6920259f936b372c3e632
Joel Hansson <joel.hasson@ecraft.com>
2017-06-14 13:00:01 +0200

    increase tmux history limit

--- snippets/setup-debian.sh
+++ snippets/setup-debian.sh
@@ -14,7 +14,8 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y \
   mosh vim-nox procps tcpdump nmap build-essential \
-  tmux unattended-upgrades git zsh
+  tmux unattended-upgrades git zsh \
+  openvpn openconnect
 
 confirm "install docker from https://docs.docker.com/engine/installation/linux/ubuntu/#install-using-the-repository"
 confirm "install chruby, ruby-install and nvm, google site:github.com for 'em!"
