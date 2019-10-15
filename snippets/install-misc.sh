23f36c521016b9d1892dc7b17e90fb50cefb0588
Joel Hansson <joel@tool.builders>
2019-10-15 11:45:07 +0200

    updates

--- snippets/install-misc.sh
+++ snippets/install-misc.sh
@@ -7,6 +7,7 @@ KERNEL_NAME=$(uname -s)
 K9S_VERSION=0.8.4
 DOCKER_COMPOSE_VERSION=1.24.1
 CTOP_VERSION=0.7.2
+KUBECTX_VERSION=0.6.3
 
 
 usage()
@@ -22,6 +23,7 @@ main()
   install_k9s
   install_docker_compose
   install_ctop
+  install_kubectx
 }
 
 setup()
@@ -32,6 +34,10 @@ setup()
 
 install_k9s()
 {
+  if [ $(command -v k9s | wc -l) -gt 0 ] ; then
+    echo "k9s already installed"
+    return 0
+  fi
   pushd $HOME/Downloads
   curl -OL https://github.com/derailed/k9s/releases/download/$K9S_VERSION/k9s_${K9S_VERSION}_${KERNEL_NAME}_${MACHINE_TYPE}.tar.gz
   tar xf k9s_${K9S_VERSION}_${KERNEL_NAME}_${MACHINE_TYPE}.tar.gz
@@ -41,14 +47,35 @@ install_k9s()
 
 install_docker_compose()
 {
+  if [ $(command -v docker-compose | wc -l) -gt 0 ] ; then
+    echo "docker-compose already installed"
+    return 0
+  fi
   curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-${KERNEL_NAME}-${MACHINE_TYPE} -o $HOME/bin/docker-compose
   chmod +x $HOME/bin/docker-compose
 }
 
 install_ctop()
 {
+  if [ $(command -v ctop | wc -l) -gt 0 ] ; then
+    echo "ctop already installed"
+    return 0
+  fi
   curl -L https://github.com/bcicen/ctop/releases/download/v${CTOP_VERSION}/ctop-${CTOP_VERSION}-${KERNEL_NAME,,}-${ARCH} -o $HOME/bin/ctop
   chmod +x $HOME/bin/ctop
+}
+
+install_kubectx()
+{
+  if [ $(command -v kubectx | wc -l) -gt 0 ] ; then
+    echo "kubectx already installed"
+    return 0
+  fi
+  pushd $HOME/Downloads
+  curl -OL https://github.com/ahmetb/kubectx/archive/v${KUBECTX_VERSION}.tar.gz
+  tar -xf v${KUBECTX_VERSION}.tar.gz
+  mv kubectx-${KUBECTX_VERSION}/kubectx $HOME/bin/kubectx
+  popd
 }
 
 
