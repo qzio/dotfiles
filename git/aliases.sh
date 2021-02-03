2d42c68acdd25239043df3f8252d00efe6d00d4a
Joel Hansson <joel@tool.builders>
2021-02-03 13:42:01 +0100

    mm

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,3 +1,4 @@
 alias git-fetch-and-rebase='git fetch --prune --tags --all && git co master && git rebase origin/master'
 alias git-fupstream='git co master && git rebase -i upstream/master && git push origin master'
 alias glog='git log --pretty=format:"%h %s" --graph'
+alias gp='git push'
