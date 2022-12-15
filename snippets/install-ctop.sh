dc4993cb245a40d9af7b3bb0a1dcf895bf71eeb1
joel hansson <joel.hansson@dulvie.se>
2022-12-15 21:00:03 +0100

    fixes install-ctop.sh

--- snippets/install-ctop.sh
+++ snippets/install-ctop.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -eu
 CTOP_VERSION=0.7.7
-KERNEL_NAME=$(uname -s)
+KERNEL_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
 CPU=$(uname -m)
 ARCH="amd64"
 if [ "$CPU" != "x86_64" ] ; then
   ARCH=arm64
 fi
 
-curl -L -o $HOME/bin/ctop https://github.com/bcicen/ctop/releases/download/v${CTOP_VERSION}/ctop-${CTOP_VERSION}-${KERNEL_NAME,,}-${ARCH}
+
+curl -L -o $HOME/bin/ctop https://github.com/bcicen/ctop/releases/download/v${CTOP_VERSION}/ctop-${CTOP_VERSION}-${KERNEL_NAME}-${ARCH}
+chmod +x $HOME/bin/ctop
